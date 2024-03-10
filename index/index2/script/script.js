d7721ee8926b4567fe57de2401c7a373eb917b14
Nursultan <alexing90l@gmail.com>
2024-03-10 19:07:54 +0600

    index erkekche buttu

--- index/index2/script/script.js
+++ index/index2/script/script.js
@@ -3,12 +3,10 @@ let med = document.getElementById('media-menu');
 let medblack = document.getElementById('media-black')
 function MedIcon(){
     med.style.display='block';
-    medblack.style.left='0'
-    medblack.style.transition='1s'
+    medblack.style.right='0'
 }
 function MedIcons(){
-    medblack.style.transition='1s'
-    medblack.style.left='-260px'
+    medblack.style.right='-260px'
     setTimeout(function (){
         med.style.display='none';
     },1000)
