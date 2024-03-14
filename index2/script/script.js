3372c5d5d331bfc75ff54ad69c3475b116a2d4c8
Nursultan <alexing90l@gmail.com>
2024-03-14 21:07:32 +0600

    INdex chykty jdnd buttu

--- index2/script/script.js
+++ index2/script/script.js
@@ -4,8 +4,10 @@ let medblack = document.getElementById('media-black')
 function MedIcon(){
     med.style.display='block';
     medblack.style.right='0'
+    medblack.style.transition='1s'
 }
 function MedIcons(){
+    medblack.style.transition='1s'
     medblack.style.right='-260px'
     setTimeout(function (){
         med.style.display='none';
